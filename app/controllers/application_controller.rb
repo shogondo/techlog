4991026b4f76f70f171cd92d804a0c09b7d0ce62
Shou Takenaka <sasaminn@gmail.com>
2014-08-27 16:34:06 +0900

    Add exception handler for ResourceNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ResourceNotFound, with: :render_404
+
+  private
+
+  def render_404
+    render template: "shared/404", status: :not_found
+  end
 end
