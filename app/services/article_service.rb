79e7a0fc10fe91498978f1927a8f2a17c38f20fb
Shou Takenaka <sasaminn@gmail.com>
2014-08-27 13:12:58 +0900

    Implement error handling

--- app/services/article_service.rb
+++ app/services/article_service.rb
@@ -1,5 +1,12 @@
 class ArticleService
   def find(params)
-    return "xxx"
+    time = Time.at(params[:id])
+    dir = Pathname.new(Settings[:article][:path]).join(time.year.to_s)
+    path = Dir::glob(dir.join("#{params[:id]}_*.md").to_s).first
+    if path
+      File.read(path).chomp
+    else
+      fail ResourceNotFound
+    end
   end
 end
