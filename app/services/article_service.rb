85c83abd5deb2c17712a48a3f5415335bb8ddd40
Shou Takenaka <sasaminn@gmail.com>
2014-08-27 15:48:07 +0900

    Force to convert id to integer

--- app/services/article_service.rb
+++ app/services/article_service.rb
@@ -1,6 +1,6 @@
 class ArticleService
   def find(params)
-    time = Time.at(params[:id])
+    time = Time.at(params[:id].to_i)
     dir = Pathname.new(Settings[:article][:path]).join(time.year.to_s)
     path = Dir::glob(dir.join("#{params[:id]}_*.md").to_s).first
     if path
