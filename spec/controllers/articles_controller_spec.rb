023bd93d16377f3b933e3303d1ead48d7b8ea02e
Shou Takenaka <sasaminn@gmail.com>
2014-08-27 16:21:38 +0900

    Add new test for ArticlesController#show

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe ArticlesController, :type => :controller do
+describe ArticlesController do
+  describe "GET /articles/:id" do
+    subject { get :show, id: "id" }
 
+    let(:service) { double :service }
+
+    before { expect(ArticleService).to receive(:new).and_return(service) }
+
+    context "when article found" do
+      let(:content) { {} }
+
+      before { expect(service).to receive(:find).and_return(content) }
+
+      it "responds 200 ok" do
+        subject
+        expect(response.status).to eq 200
+      end
+
+      it "renders show template" do
+        subject
+        expect(response).to render_template :show
+      end
+
+      it "assigns found article to @content" do
+        subject
+        expect(assigns[:content]).to be content
+      end
+    end
+  end
 end
