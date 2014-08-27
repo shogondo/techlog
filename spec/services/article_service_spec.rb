c81ddcb7bfe01f8a64f222b0dae00657c37782a7
Shou Takenaka <sasaminn@gmail.com>
2014-08-27 13:21:18 +0900

    Add new test case of #find

--- spec/services/article_service_spec.rb
+++ spec/services/article_service_spec.rb
@@ -13,6 +13,11 @@ describe ArticleService do
       it { is_expected.to eq "xxx" }
     end
 
+    context "when id is not unix epoch" do
+      let(:id) { "id" }
+      it { expect{subject}.to raise_error ResourceNotFound }
+    end
+
     context "when article not found" do
       let(:id) { 0 }
       it { expect{subject}.to raise_error ResourceNotFound }
